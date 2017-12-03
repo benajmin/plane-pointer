e802d7eda6c322cdbdf30fff3c5afe9536e5d7a4
Benjamin Wilkins <wilkben@yahoo.ca>
2017-12-03 12:03:50 -0500

    added support for servos

--- arduino_pointer/arduino_pointer.ino
+++ arduino_pointer/arduino_pointer.ino
@@ -6,12 +6,22 @@
  *  https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
  */
 
+#include <Servo.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#define YAW_SERVO_PIN 5
+#define PITCH_SERVO_PIN 6
+
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
+Servo yaw_servo;
+Servo pitch_servo;
+
 void setup() {
+  yaw_servo.attach(YAW_SERVO_PIN);
+  pitch_servo.attach(PITCH_SERVO_PIN);
+  
   Serial.begin(9600);
   lcd.begin(16,2);
 
